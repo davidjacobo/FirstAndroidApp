69537e97fa3dbc12861527b39cf966f2eeb89565
David Jacobo <jguillen@cimat.mx>
2015-10-21 00:34:21 -0500

    Adding second activity

--- app/src/main/java/com/example/jguillen/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/jguillen/firstandroidapp/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.jguillen.firstandroidapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.jguillen.firstandroidapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        //Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+    }
+
 }
